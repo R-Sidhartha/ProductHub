0486ffc5b968123a5b7f9956bddd347585b40748
R-Sidhartha <rsidhartha20@iitk.ac.in>
2025-05-25 21:16:04 +0530

    Update the backend api url

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://producthub-server.onrender.com";
 
 export async function login(email: string, password: string) {
   const res = await fetch(`${BASE_URL}/api/auth/login`, {
--- lib/api/products.ts
+++ lib/api/products.ts
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://producthub-server.onrender.com";
 
 function getAuthHeaders(token: string) {
   return {
